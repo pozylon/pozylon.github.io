f5e7937b473a67006693c8f0713cac73f8c55cc6
Pascal Kaufmann <kaufmae@me.com>
2017-11-14 17:36:36 +0100

    deploy

---
+++ _next/7d05d58d-e853-4578-99c3-2452b4bdde8f/page/_error/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/_error', function() {
+            var comp = module.exports=webpackJsonp([4],{207:function(e,t,n){e.exports=n(208)},208:function(e,t,n){"use strict";function u(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var r=n(13),o=u(r),a=n(4),l=u(a),f=n(5),i=u(f),d=n(14),c=u(d),s=n(15),p=u(s),_=n(1),v=u(_),h=function(e){function t(){return(0,l.default)(this,t),(0,c.default)(this,(t.__proto__||(0,o.default)(t)).apply(this,arguments))}return(0,p.default)(t,e),(0,i.default)(t,[{key:"render",value:function(){return v.default.createElement("div",null,"404")}}]),t}(v.default.Component);h.getInitialProps=function(e){var t=e.req,n=e.res;return t&&n.writeHead(302,{Location:"/"}),{}},t.default=h}},[207]);
+            return { page: comp.default }
+          })
+        
